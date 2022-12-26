f8d5cc43a53290088ab7c56dd0eb7062891ff85a
WadcharakornSomthawin <99709123+WadcharakornSomthawin@users.noreply.github.com>
2022-12-26 11:12:51 +0700

    Add files via upload

---
+++ birtdaylist/src/List.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const List = (props) => {
+  const {people} = props;
+//  return (
+//    <div>
+//      {
+//        for(let i=0;i<people.length; i++){
+
+//        }
+//      }
+//      <article className="person">
+//        <img src="" alt="profile image"/>
+//        <div>
+//          <h4>name</h4>
+ //         <p> 29 years old</p>
+ //       </div>
+ //     </article>
+ //   </div>
+ // );
+
+  return(
+    <div>
+      {
+        people.map((person)=>{
+          const {id, name, age, image}= person;
+          return(
+            <article className="person" key={id}>
+        <img src={image} alt="profile image"/>
+        <div>
+          <h4>{name}</h4>
+          <p> {age} years old</p>
+        </div>
+      </article>
+          )
+        }
+
+        )
+      }
+    </div>
+  )
+};
+
+export default List;
