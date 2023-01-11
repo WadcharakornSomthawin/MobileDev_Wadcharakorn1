c2b58afe4f8346599721100c44254f03fd8f0bdd
Wadcharakorn Somthawin <wadcharakorn030345@gmail.com>
2023-01-11 17:14:15 +0700

    update authjwt.js

--- REST-API_SQL_AUTH/middleware/authjwt.js
+++ REST-API_SQL_AUTH/middleware/authjwt.js
@@ -5,7 +5,7 @@ const db = require("../models");
 const User = db.users;
 
 verifyToken = (req, res, next) => {
-  let token = req.header["x-access-token"];
+  let token = req.headers["x-access-token"];
   if (!token) {
     return res.status(403).send({
       message: "No token provided!",
