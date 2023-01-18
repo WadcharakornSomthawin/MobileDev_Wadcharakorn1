4fb023c1d0d7ebfca5c03e479196c5b5f50e5834
Wadcharakorn Somthawin <wadcharakorn030345@gmail.com>
2023-01-18 13:22:48 +0700

    frontend

---
+++ grab-restaurants-react-auth/src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Card = ({restaurant, handleDelete}) => {
+  return (
+    <div className="card" style={{ width: "18rem" }} key={restaurant.id}>
+      <img src={restaurant.imageurl} className="card-image-top" alt="" />
+      <div className="card-body">
+        <h5 className="title">{restaurant.name}</h5>
+        <p className="card-text">{restaurant.type}</p>
+        <Link
+          to=""
+          className="btn btn-danger px-2"
+          onClick={() => handleDelete(restaurant.id)}
+        >
+          Delete
+        </Link>
+        <Link to={"/update/" + restaurant.id} className="btn btn-warning px-2">
+          Edit
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Card
