2cfe4e1bb588b4d2b3fbdd90bac6d1a6608c5a43
Wadcharakorn Somthawin <wadcharakorn030345@gmail.com>
2023-01-25 16:41:36 +0700

    redux-counter

---
+++ redux-counter/redux-counter/src/components/Counter.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, incrementByAmount, reset } from "../reducers/counterReducer";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0);
+  const value = Number(amount) || 0;
+
+  const resetAll =()=>{
+    setAmount(0);
+    dispatch(reset())
+  }
+
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          -
+        </button>
+      </div>
+      <input
+        type="text"
+        name="amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={()=>{dispatch(incrementByAmount(value))}}>Add amount</button>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
