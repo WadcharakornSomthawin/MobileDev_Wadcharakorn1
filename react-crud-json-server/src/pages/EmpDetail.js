84d395fe26a9c0520214f9e9fa13df0033a8b1fc
WadcharakornSomthawin <99709123+WadcharakornSomthawin@users.noreply.github.com>
2023-02-28 21:40:45 +0700

    edit EmpDetail

--- react-crud-json-server/src/pages/EmpDetail.js
+++ react-crud-json-server/src/pages/EmpDetail.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const EmpDetail = () => {
-  return (
-    <div>EmpDetail</div>
-  )
-}
+  const [emData, setEmData] = useState(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/employee")
+      .then((res) => {
+        console.log(res);
+        setEmData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-export default EmpDetail
+  const loadEdit = (id) => {
+    navigate("/employee/edit/" + id);
+  };
+  const loadDetail = (id) => {
+    navigate("/employee/detail/" + id);
+  };
+  const removeEmp = (id) => {
+    if (window.confirm("Do you want to delete this  employee?")) {
+      axios
+        .delete("http://localhost:8000/employee/" + id)
+        .then((res) => {
+          alert("Remove successfuly.");
+          window.location.reload();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-title">
+          <h2>Employee List</h2>
+        </div>
+        <div className="card-body">
+          <Link
+            to="/employee/create"
+            className="btn btn-success"
+            style={{ float: "left" }}
+          >
+            Add new (+)
+          </Link>
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>ACtion</td>
+              </tr>
+            </thead>
+            <tbody>
+              {emData &&
+                emData.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.phone}</td>
+                      <td>
+                        <a
+                          className="btn btn-success"
+                          onClick={() => {
+                            loadEdit(item.id);
+                          }}
+                        >
+                          Edit
+                        </a>{" "}
+                        <a
+                          className="btn btn-danger"
+                          onClick={() => {
+                            removeEmp(item.id);
+                          }}
+                        >
+                          Remove
+                        </a>{" "}
+                        <a
+                          className="btn btn-primary"
+                          onClick={() => {
+                            loadDetail(item.id);
+                          }}
+                        >
+                          Detail
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmpDetail;
